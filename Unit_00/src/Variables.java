e35cb72c0ca3e9570690093c437b1bc067f5c840
changers2100 <ethanchangliang@gmail.com>
2022-01-17 21:54:35 -0800

    final edits

--- Unit_00/src/Variables.java
+++ Unit_00/src/Variables.java
@@ -1,10 +1,10 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
 
-//Name -
-//Date -
-//Class -
-//Lab  -
+//Name - Ethan Chang
+//Date - 1/17/2022
+//Class - CSA p1
+//Lab  - Variables 
 
 public class Variables
 {
@@ -16,30 +16,39 @@ public class Variables
 		//float		double
 		//char      boolean		String
 
+		
 		//integer variables
 		byte byteOne = 127;
-
+		short shortOne = -312;
+		int intOne = 101222;
+		long longne = 1111112222;
+		
 
 		//decimal variables
-
+		double doubleOne = 20.8555555;
+		float floatOne = 19.6f;
 
 		//other integer types
-
+		char charOne = 'a';
+		boolean booleanOne = true;
+		String StringOne = "Ethan";
 
 		//other types
-
-
 		//output your information here
 		System.out.println("/////////////////////////////////");
 		System.out.println("*                               *");
 		System.out.println("*        integer types          *");
 		System.out.println("*                               *");
-		System.out.println("*8 bit - byteOne = "+byteOne+"\t\t*");
+		System.out.println("*08 bit - byteOne = "+byteOne+"\t\t*");
+		System.out.println("*16 bit - shortOne = "+shortOne+"\t\t*");
+		System.out.println("*32 bit - intOne = "+byteOne+"\t\t*");
+		System.out.println("*64 bit - longOne = "+shortOne+"\t\t*");
 
-
-
-
-
+		
+		System.out.println("*32 bit - doubleOne = "+doubleOne+"\t\t*");
+		System.out.println("*64 bit - floatOne = "+floatOne+"\t\t*");
+		
+		System.out.println("*16 bit - StringOne = "+StringOne+"\t\t*");
 
 
 
