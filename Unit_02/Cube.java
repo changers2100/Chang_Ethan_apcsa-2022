cbd5882bf800b47489f6414315423af29408ca65
changers2100 <97621434+changers2100@users.noreply.github.com>
2022-01-20 08:56:32 -0800

    new

--- Unit_02/Cube.java
+++ Unit_02/Cube.java
@@ -5,20 +5,9 @@
 
 public class Cube
 {
-	private double side;
-	private double surfaceArea;
 	
-	public void sideOne() {
-		side = 130;
-	}
-	
-	public void surfaceArea() {
-		surfaceArea = 6* Math.pow(side, 2);
-	}
 	public static double area( int side )
 	{
-		System.out.print("The surface area is: ");
-		System.out.print(surfaceArea);
-		System.out.println();
+		return 6*side*side;
 	}
 }
