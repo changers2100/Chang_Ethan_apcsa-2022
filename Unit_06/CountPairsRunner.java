8f2dfd4ae5f7f31efedb367e76c425707d054adc
changers2100 <97621434+changers2100@users.noreply.github.com>
2022-02-01 09:21:48 -0800

    updates

--- Unit_06/CountPairsRunner.java
+++ Unit_06/CountPairsRunner.java
@@ -9,6 +9,11 @@ public class CountPairsRunner
 	public static void main( String[] args )
 	{
 		System.out.println( CountPairs.pairCounter("test_cases") );
+		System.out.println( CountPairs.pairCounter("ddogccatppig") );
+		System.out.println( CountPairs.pairCounter("dogcatpig") );
+		System.out.println( CountPairs.pairCounter("xxyyzz"));
+		
+
 		//add in all of the provided test cases from the lab handout	
 	}
 }
