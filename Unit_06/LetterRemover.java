4e9fa7bc197007444d55d2059911c87c50f238e2
changers2100 <ethanchangliang@gmail.com>
2022-02-01 16:29:04 -0800

    new

--- Unit_06/LetterRemover.java
+++ Unit_06/LetterRemover.java
@@ -11,11 +11,14 @@ public class LetterRemover
 
 	public LetterRemover()
 	{
-		//call set
+		setRemover("",' ');
 	}
 
 	//add in second constructor
-	
+	public LetterRemover(String s, char rem)
+	{
+		setRemover(s, rem);
+	}
 	
 	public void setRemover(String s, char rem)
 	{
@@ -25,12 +28,19 @@ public class LetterRemover
 
 	public String removeLetters()
 	{
-		String cleaned=sentence;
+		String look = String.valueOf(lookFor);
+		String cleaned = "";
+		
+		for (int i=0; i<sentence.length(); i++) {
+			if(sentence.charAt(i)!= lookFor) {
+				cleaned += sentence.charAt(i);
+			}
+		}
 		return cleaned;
 	}
 
 	public String toString()
 	{
-		return sentence + " - letter to remove " + lookFor;
+		return sentence + " - letter to remove " + lookFor +"\n" + removeLetters() +"\n\n";
 	}
 }
--- Unit_06/LetterRemoverRunner.java
+++ Unit_06/LetterRemoverRunner.java
@@ -9,6 +9,10 @@ public class LetterRemoverRunner
 	public static void main( String args[] )
 	{
 		//add test cases		
+		LetterRemover test = new LetterRemover();
+		
+		test.setRemover("I am Sam I am", 'a');
+		out.println(test);
 											
 	}
 }
