5609de66052a73b069e1bd52805feedf02572415
changers2100 <97621434+changers2100@users.noreply.github.com>
2022-02-10 08:44:43 -0800

    ray sum

--- Unit_08/RaySumLast.java
+++ Unit_08/RaySumLast.java
@@ -7,14 +7,20 @@ public class RaySumLast
 {
 	public static int go(int[] ray)
 	{
-		if (ray.length() ==0)
-			return -1;
-		int val = ray[ ray.length()-1 ];
-		in
-		for(int i = 0; i < ray.length-1, i++)
+		int sum = 0;
+		for(int i = 0; i < ray.length-1; i++)
 		{
-			if (ray[i] > val )
-					sum sum rau[i]
+			if (ray[i] > ray[ray.length-1] ) {
+					sum += ray[i];
 		}
 	}
+	if(sum >0) {
+		return sum;
+	}
+	else {
+		return -1;
+	}
+		
+	}
+	
 }
