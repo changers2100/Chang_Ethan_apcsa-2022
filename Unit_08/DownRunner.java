c5a337e619e09f1843f7eed344a6c14afe21dd79
changers2100 <ethanchangliang@gmail.com>
2022-02-09 23:41:44 -0800

    rays

--- Unit_08/DownRunner.java
+++ Unit_08/DownRunner.java
@@ -6,6 +6,7 @@
 public class DownRunner
 {
 	public static void main( String args[] )
-	{											
+	{				
+		System.out.println( RayDown.go(new int[]{-99,1,2,3,4,5,6,7,8,9,10,5}));
 	}
 }
--- Unit_08/RayOddToEven.java
+++ Unit_08/RayOddToEven.java
@@ -5,8 +5,39 @@
 
 public class RayOddToEven
 {
-	public static boolean go(int[] ray)
+	private int[] ray;
+	
+	public RayOddToEven()
 	{
-		return false;
+		setRay(new int[] {0, 0});
+	}
+	
+	public RayOddToEven(int[] inputRay)
+	{
+		setRay(inputRay);
+	}
+	
+	public void setRay(int[] inputRay) {
+		ray = inputRay;
+	}
+	public int go(int[] ray)
+	{
+		int oddCount;
+		int evenCount;
+		for(int i = 0; i<ray.length; i++) {
+			if(ray[i] %2 != 0) {
+				oddCount = i;
+				for(int even = i; even <ray.length; even++)
+				{
+					if(ray[even] %2 == 0) {
+						evenCount = even;
+						return evenCount-oddCount;
+					}
+				}
+			}
+		}
+		return -1;
 	}
 }
+
+//what does an array return if index is not in string -- -1?
