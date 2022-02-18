87b25301c1c722d88eca5cea5ccc10e6875ecc01
changers2100 <97621434+changers2100@users.noreply.github.com>
2022-02-18 09:20:25 -0800

    new elevens

---
+++ elevens2/src/CardTester.java
@@ -0,0 +1,24 @@
+ * This is a class that tests the Card class.
+ */
+package activity1;
+public class CardTester {
+
+	/**
+	 * The main method in this class checks the Card operations for consistency.
+	 *	@param args is not used.
+	 */
+	public static void main(String[] args) {
+		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		
+		Card aceHearts1 = new Card("ace", "hearts", 1);
+		Card kingSpades13 = new Card("king", "spades", 13);
+		Card fourClubs4 = new Card("four", "clubs", 4);
+		
+		System.out.print(aceHearts1().match(kingSpades13))
+		
+		System.out.print(aceHearts1);
+		System.out.print(kingSpades13);
+		System.out.print(fourClubs4);
+
+	}
+}
