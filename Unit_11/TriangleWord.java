fd861ad3a3dc02c0b64bcbcc0303b82d03f0a585
changers2100 <ethanchangliang@gmail.com>
2022-02-28 23:03:46 -0800

    triangle word

--- Unit_11/TriangleWord.java
+++ Unit_11/TriangleWord.java
@@ -1,15 +1,17 @@
-//(c) A+ Computer Science
-//www.apluscompsci.com
-//Name -
-
 import java.util.Scanner;
 import static java.lang.System.*;
 
 public class TriangleWord
 {
-	//instance variables and constructors could be present, but are not necessary
-		
 	public static void printTriangle(String word)
 	{
+		for(int i = 0; i<word.length(); i++) {
+			for(int j = 0; j<=i+1; j++){
+				for(int k = 0; k<= i; k++) {
+					System.out.print(word.charAt(k));
+				}
+			}
+			System.out.println();
+		}
 	}
 }
