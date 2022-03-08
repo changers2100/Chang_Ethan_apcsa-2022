0542efa3e2bc5cb00fca27b96d2e4b06c21383e6
changers2100 <ethanchangliang@gmail.com>
2022-03-07 23:54:18 -0800

    a

--- Unit_12/WordRunner.java
+++ Unit_12/WordRunner.java
@@ -12,10 +12,21 @@ public class WordRunner
 {
 	public static void main( String args[] ) throws IOException
 	{
-		Scanner file = new Scanner(new File("words.dat"));
+		Scanner file = new Scanner(new File("C:\\Users\\Brothers\\Desktop\\Chang_Ethan_apcsa-2022\\\\Unit_12\\\\words.dat\"));
 
 		int size = file.nextInt();
 		file.nextLine();
+		
+		Word[] words = new Word[size];
+		for(int i = 0; i< words.length; i++) {
+			words[i] = new Word(file.nextLine());
+		}
+		
+		Arrays.sort(words);
+		
+		for(Word word : words) {
+			out.println();
+		}
 		
 
 
