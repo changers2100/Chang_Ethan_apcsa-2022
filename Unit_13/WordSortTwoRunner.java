a77becfece6e1e4751f14a3306c963338f061230
changers2100 <ethanchangliang@gmail.com>
2022-03-30 21:51:12 -0700

    unit_13

--- Unit_13/WordSortTwoRunner.java
+++ Unit_13/WordSortTwoRunner.java
@@ -12,13 +12,16 @@ public class WordSortTwoRunner
 {
 	public static void main( String args[] ) throws IOException
 	{
-		Scanner file = new Scanner(new File("wordsorttwo.dat"));
+		Scanner file = new Scanner(new File("C:\\Users\\Brothers\\OneDrive\\Desktop\\Chang_Ethan_apcsa-2022\\Unit_13\\wordsorttwo.dat"));
 		int size = file.nextInt();
 		file.nextLine();
 		for(int i = 0; i<size; i++)
 		{
 			String sentence = file.nextLine();
 			//instantiate a new WordSort
+			WordSortTwo test = new WordSortTwo(sentence);
+			test.sort();
+			System.out.print(test);
 		}
 	}
 }
